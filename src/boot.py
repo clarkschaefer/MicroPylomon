b74b92f2e1ec7ca6d601623969c113579025f5bc
Clark Schaefer <clark.h.schaefer@gmail.com>
2024-02-08 23:53:41 -0800

    minor fixes, start adding ble uuids

--- src/boot.py
+++ src/boot.py
@@ -1,10 +1,39 @@
-import uasyncio as asyncio
-import struct
+"""
+fully sniff a Peloton Bike (original)'s level-shifted RS-232 and spit that data out as if we're a bike computer
+"""
 
-import esp32
+import uasyncio as asyncio
+import aioble
+import bluetooth
+import struct
+import esp32 # we don't need this until I do the NVS stuff technically but whatever
 
 from binascii import hexlify
 from machine import UART
+from micropython import const
+
+# ===== SERVICES =====
+# cycling power service
+# needs: notifications, read/write characteristic descriptions
+_uuid_cycle_power_service = bluetooth.UUID(0x1818)
+# cycling speed and cadence service
+# needs: notifications, read/write characteristic descriptions
+_uuid_cycle_cadence_speed_service = bluetooth.UUID(0x1816)
+
+# ===== CHARACTERISTICS =====
+# cycling power feature, mandatory read
+_uuid_cycling_power_feature = bluetooth.UUID(0x2A65)
+# cycling power measurement, mandatory notify
+_uuid_cycling_power_measurement = bluetooth.UUID(0x2A63)
+# sensor location, mandatory read
+_uuid_cycling_sensor_location = bluetooth.UUID(0x2A5D)
+# csc measurement, mandatory notify
+_uuid_csc_measurement = bluetooth.UUID(0x2A5B)
+# csc feature, mandatory read
+_uuid_csc_feature = bluetooth.UUID(0x2A5C)
+
+# ===== DESCRIPTORS =====
+# 
 
 
 def normalize_resistance(resistance):
@@ -24,7 +53,7 @@ def calc_speed(power, resistances_array):
     pass
 
 
-def flip_le_ascii(s):
+def reverse_bytes_to_int(s):
     """
     decode a bytestring as ascii, then reverse it and return it as an int
     maybe there's a more efficient way to be unpacking the structs?
@@ -58,7 +87,7 @@ async def serial_mirror(buf, uartA, uartB, packetready_evt):
             packetready_evt.set()
         
         len = 0
-        await asyncio.sleep_ms(1)
+        await asyncio.sleep_ms(0)
 
 
 async def nvs_manager():
@@ -66,6 +95,7 @@ async def nvs_manager():
     stash values when asked by the packet parser, checking if they've changed
     that would mean we're on a different peloton
     """
+
 
 async def packet_parse(buf, packetready_evt):
     """
@@ -97,19 +127,19 @@ async def packet_parse(buf, packetready_evt):
             
             elif buf[1] == 0xF7:
                 print("bike resistance cal value")
-                print(flip_le_ascii(struct.unpack("<4s", buf[3:7])[0]))
+                print(reverse_bytes_to_int(buf[3:7]))
             
             elif buf[1] == 0x41:
                 print("bike cadence response")
-                print(flip_le_ascii(struct.unpack("<3s", buf[3:6])[0]))
+                print(reverse_bytes_to_int(buf[3:6]))
             
             elif buf[1] == 0x44:
                 print("bike power response")
-                print(flip_le_ascii(struct.unpack("<5s", buf[3:8])[0]))
+                print(reverse_bytes_to_int(buf[3:8]))
             
             elif buf[1] == 0x4A:
                 print("bike raw resistance response")
-                print(flip_le_ascii(struct.unpack("<4s", buf[3:7])[0]))
+                print(reverse_bytes_to_int(buf[3:7]))
         
         elif buf[0] == 0xFE:
             print("head bootup message")
@@ -131,7 +161,7 @@ async def packet_parse(buf, packetready_evt):
             print("head getting resistance cal value")            
 
         else:
-            raise Exception("Unknown message" + str(buf))
+            raise Exception("Unknown message", str(buf))
 
         packetready_evt.clear()
         await asyncio.sleep(0)
